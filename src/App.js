51478b73020f20ce4290b09bbaf7bdb7c59647ea
Matleena Autio <c0auma00@students.oamk.fi>
2021-09-14 16:12:58 +0300

    heartRate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from 'react';
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function calculate(e) {
+    e.preventDefault();
+    setResult((220-age) * 0.85)
+    // setResult((220-age) * 0.65)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={calculate}>
+      <h3>Heart rate limits calculator</h3>
+      <div>
+        <label>Age</label>
+        <input type="number" step="0.01"
+          onChange={e => setAge(e.target.value)}
+          value={age}/>
+      </div>
+      <div>
+        <label>Heart rate limits</label>
+        <input type="number" 
+          onChange={e => setResult(e.target.value)}
+          value={result}/>
+      </div>
+      <div>
+      <button type="button" class="btn btn-primary">Calculate</button>
+      </div>
+    </form>
   );
 }
 
